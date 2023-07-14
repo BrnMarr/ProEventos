e651cfff00fede9f668736d5773bf2100c505890
Bruno Martins <Bruno Martins>
2023-07-14 14:18:07 -0300

    sqlite

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using ProEventos.API.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using Back.src.ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
@@ -13,51 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-         
-        public EventoController()
+             private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-           
+           _context = context;
         }
-
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento() {
-               EventoId = 1,
-               Tema = "Angular 11",
-               Local = "BH",
-               Lote = "1º Lote",
-               QtdPessoas = 250,
-               DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-            },
-              new Evento() {
-               EventoId = 2,
-               Tema = ".Net",
-               Local = "SP",
-               Lote = "2º Lote",
-               QtdPessoas = 350,
-               DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy"),
-            }  ,
-              new Evento() {
-               EventoId = 3,
-               Tema = ".Net Core",
-               Local = "RJ",
-               Lote = "1º Lote",
-               QtdPessoas = 250,
-               DataEvento = DateTime.Now.AddDays(7).ToString("dd/MM/yyyy"),
-            }           
-        };
-
-
-
+     
+        
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-           return _evento;
+           return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetById(int id)
         {
-           return _evento.Where(evento => evento.EventoId == id);
+           return _context.Eventos.Where(evento => evento.EventoId == id);
         }
     }
 }
